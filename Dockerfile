6a0a9d2cbff47036e45669f524c68c3ecdb631e7
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-03-19 15:10:55 +0100

    docker image ready

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM openjdk:20-jdk-slim-buster
 ENV DATAPLATFORM_IP=localhost
 
 WORKDIR /app
-COPY ./target/tracabGen5Filter-0.0.1-SNAPSHOT.jar /app
+COPY ./target/tracabgen5filter-0.0.1-SNAPSHOT.jar /app
 
 EXPOSE 8080
 
-CMD ["java", "-jar", "tracabGen5Filter-0.0.1-SNAPSHOT.jar"]
+CMD ["java", "-jar", "tracabgen5filter-0.0.1-SNAPSHOT.jar"]
--- dockerize.cmd
+++ dockerize.cmd
@@ -1,6 +1,8 @@
-docker build -t tracabGen5Filter:0.0.1 -t tracabGen5Filter:latest .
+REM docker build -t tracabgen5filter:0.0.1 -t tracabgen5filter:latest .
+docker rmi tracabgen5filter:latest -f
+docker build -t tracabgen5filter:latest .
 
 REM if it runs on localhost
-REM docker run -it -p 8080:8080 tracabGen5Filter:latest
+REM docker run -it -p 8080:8080 tracabgen5filter:latest
 REM if it runs on 192.168.1.100
-REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 tracabGen5Filter:latest
+REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 tracabgen5filter:latest
